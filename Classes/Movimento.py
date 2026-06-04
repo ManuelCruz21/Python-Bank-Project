4f38c7218e33bc91d5ebbf390d0a4fad78a0c015
ManuelCruz21 <manuelhacruz@hotmail.com>
2026-06-04 12:11:55 +0100

    class Moviento creation

---
+++ Classes/Movimento.py
@@ -0,0 +1,14 @@
+class Movimento:
+
+    def __init__(self, tipo, valor, data, descricao,conta_destino):
+        self._tipo = tipo
+        self._valor = valor
+        self._data = data
+        self._descricao = descricao
+        self._conta_destino = conta_destino
+
+    def __str__(self):
+        return f"Tipo: {self._tipo}, Valor: {self._valor}, Data: {self._data}, Descrição: {self._descricao}, Conta Destino: {self._conta_destino}"
+    
+    
+
